e8fea19803af2f3e82fccea084358607eb664c3b
mflarup@gmail.com <mflarup@gmail.com>
2016-08-25 14:23:09 +0200

    HAHHAHH

--- test.sh
+++ test.sh
@@ -20,7 +20,6 @@ function testString {
 testString "Finn should say ADVENTURE TIME" ./finn.sh 'ADVENTURE TIME'
 testString "Jake should say Mathemagical" ./jake.sh 'Mathemagical'
 testString "HELLO MISTER. I DO NOT GET THESE SHELL COMMANDS" ./mark.sh 'SOMETHING'
-testString "Ooooh, I may be in way over my head" ./automated.sh 'head'
 testString "HELLO" ./hello.sh 'HELLO'
 echo "SUCCESS!" 
 
